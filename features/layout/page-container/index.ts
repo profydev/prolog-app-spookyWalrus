fd0f563fc773c4bba698787c3508c9b8b2c1923a
Zen Nakamura <zenzenzen@gmail.com>
2023-12-02 15:43:07 -0500

    Changed footer to export as default function

--- features/layout/page-container/index.ts
+++ features/layout/page-container/index.ts
@@ -1,2 +1,1 @@
 export { PageContainer } from "./page-container";
-export { Footer } from "./footer";
--- features/layout/page-container/page-container.tsx
+++ features/layout/page-container/page-container.tsx
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import { SidebarNavigation } from "../sidebar-navigation";
 import styles from "./page-container.module.scss";
-import { Footer } from "./footer";
+import Footer from "./footer";
 
 type PageContainerProps = {
   children: React.ReactNode;
