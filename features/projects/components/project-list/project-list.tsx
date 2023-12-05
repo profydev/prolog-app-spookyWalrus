198e1318895c5e1c4ceb51792b0d8d37fe0635d5
Zen Nakamura <spookyWalrus@users.noreply.github.com>
2023-12-05 12:00:47 -0500

    Added code block to display rotating load circle, along with stylings, images. Also added functionality to click and reload api fetch call (#11)

--- features/projects/components/project-list/project-list.tsx
+++ features/projects/components/project-list/project-list.tsx
@@ -2,9 +2,11 @@ import { ProjectCard } from "../project-card";
 import { useGetProjects } from "../../api/use-get-projects";
 import styles from "./project-list.module.scss";
 import Image from "next/image";
+import { Button } from "@features/ui";
+// import Link from "next/link";
 
 export function ProjectList() {
-  const { data, isLoading, isError, error } = useGetProjects();
+  const { data, isLoading, isError, error, refetchProjects } = useGetProjects();
 
   if (isLoading) {
     return (
@@ -24,7 +26,40 @@ export function ProjectList() {
 
   if (isError) {
     console.error(error);
-    return <div>Error: {error.message}</div>;
+    return (
+      // <div>Error: {error.message}</div>;)
+      <div className={styles.alertBox}>
+        <Image
+          src={"icons/alert-circle.svg"}
+          width={20}
+          height={20}
+          alt="alert-circle"
+        />
+        <div className={styles.content}>
+          <div className={styles.supportingText}>
+            There was a problem while loading the project data
+          </div>
+          <div className={styles.actions}>
+            <Button className={styles.button}>
+              <div className={styles.buttonBase}>
+                <div
+                  onClick={() => refetchProjects()}
+                  className={styles.tryAgain}
+                >
+                  Try Again
+                </div>
+                <Image
+                  src="icons/arrow-right.svg"
+                  width={20}
+                  height={20}
+                  alt="arrow-right"
+                />
+              </div>
+            </Button>
+          </div>
+        </div>
+      </div>
+    );
   }
 
   return (
