526e7418cb3d92e94868e2bf4be63d447a6dcc55
Zen Nakamura <spookyWalrus@users.noreply.github.com>
2023-12-23 08:01:20 -0500

    Created component, stylesheet and stories for Button (#12)
    * Created component, stylesheet and stories for Button
    
    * Removed storybook-addon-pseudo-states due to dependency conflicts. Addon not needed
    
    * Added missing onClick prop to <button> element

--- features/ui/button/button.tsx
+++ features/ui/button/button.tsx
@@ -1,9 +1,74 @@
-import { ButtonHTMLAttributes } from "react";
+import React from "react";
 import classNames from "classnames";
 import styles from "./button.module.scss";
 
-export function Button(props: ButtonHTMLAttributes<HTMLButtonElement>) {
-  return (
-    <button {...props} className={classNames(styles.button, props.className)} />
-  );
+export enum ButtonSize {
+  sm = "sm",
+  md = "md",
+  lg = "lg",
+  xlg = "xlg",
 }
+
+export enum ButtonColor {
+  primary = "primary",
+  secondary = "secondary",
+  gray = "gray",
+  empty = "empty",
+  emptyGray = "emptyGray",
+  error = "error",
+  emptyError = "emptyError",
+  default = "default",
+}
+
+export enum ButtonState {
+  default = "default",
+  hover = "hover",
+  focus = "focus",
+  disabled = "disabled",
+}
+
+interface Props {
+  state?: ButtonState;
+  size?: ButtonSize;
+  color?: ButtonColor;
+  icon?: React.ReactNode;
+  text?: string;
+  children: React.ReactNode;
+  showText?: boolean;
+  showCircle1?: boolean;
+  showCircle2?: boolean;
+  className?: string;
+  onClick?: () => void;
+}
+
+const Button = ({
+  children,
+  text,
+  state = ButtonState.default,
+  size = ButtonSize.md,
+  color = ButtonColor.default,
+  icon,
+  showText,
+  showCircle1,
+  showCircle2,
+  onClick,
+}: Props) => {
+  return (
+    <button
+      className={classNames(
+        styles.button,
+        styles[state],
+        styles[size],
+        styles[color],
+      )}
+      onClick={onClick}
+    >
+      {showCircle1 && <span className={styles.circleOne}>{icon}</span>}
+      {showText && <span className={styles.childText}>{text}</span>}
+      {showCircle2 && <span className={styles.circleTwo}>{icon}</span>}
+      {children}
+    </button>
+  );
+};
+
+export default Button;
--- features/ui/button/index.ts
+++ features/ui/button/index.ts
@@ -1,1 +1,5 @@
-export { Button } from "./button";
+// export { Button, ButtonColor, ButtonSize } from "./button";
+
+import Button, { ButtonColor, ButtonSize } from "./button";
+
+export { Button, ButtonColor, ButtonSize };
