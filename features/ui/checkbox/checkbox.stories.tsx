1de050f2fec982ada25210430b77b25a5e3b9b67
Zen Nakamura <zenzenzen@gmail.com>
2023-12-28 10:37:52 -0500

    Changed component name to fix type error; fixed syntax in stylesheet; fixed imported components in index file

---
+++ features/ui/checkbox/checkbox.stories.tsx
@@ -0,0 +1,42 @@
+import { Meta, StoryFn } from "@storybook/react";
+import Checkbox from "./checkbox";
+import { CheckSize, CheckColor } from "./checkbox";
+
+const meta: Meta<typeof Checkbox> = {
+  component: Checkbox,
+  title: "Input checkbox",
+  argTypes: { onClick: { action: "clicked" } },
+  parameters: {
+    // More on Story layout: https://storybook.js.org/docs/react/configure/story-layout
+    layout: "fullscreen",
+    viewMode: "docs",
+  },
+};
+export default meta;
+
+const Template: StoryFn<typeof Checkbox> = ({ ...args }) => (
+  <Checkbox {...args} />
+);
+
+export const Default = Template.bind({});
+Default.args = {
+  size: CheckSize.md,
+  color: CheckColor.default,
+  label: "Label",
+  checked: false,
+  indeterminate: true,
+};
+
+Default.argTypes = {
+  size: {
+    control: { type: "select" },
+    options: ["sm", "md"],
+  },
+  label: {
+    control: { control: "text" },
+  },
+  color: {
+    control: { type: "select" },
+    options: ["default", "hover", "focus", "disabled"],
+  },
+};
---
+++ features/ui/checkbox/checkbox.tsx
@@ -0,0 +1,60 @@
+import React, { useEffect, useRef } from "react";
+import classNames from "classnames";
+import styles from "./checkbox.module.scss";
+
+export enum CheckSize {
+  sm = "sm",
+  md = "md",
+}
+
+export enum CheckColor {
+  default = "default",
+  hover = "hover",
+  focus = "focus",
+  disabled = "disabled",
+}
+
+interface Props {
+  label?: string;
+  size?: CheckSize;
+  color?: CheckColor;
+  children: React.ReactNode;
+  className?: string;
+  onClick?: (event: React.MouseEvent<HTMLElement, MouseEvent>) => void;
+  onChange?: () => void;
+  checked: boolean;
+  indeterminate: boolean;
+}
+
+const Checkbox = ({
+  label,
+  size = CheckSize.md,
+  color = CheckColor.default,
+  onChange,
+  checked,
+  indeterminate,
+}: Props) => {
+  const ref = useRef<HTMLInputElement>(null);
+  useEffect(() => {
+    if (ref.current) {
+      ref.current.indeterminate = indeterminate;
+    }
+  }, [indeterminate]);
+
+  return (
+    <div className={styles.box}>
+      <input
+        type="checkbox"
+        ref={ref}
+        checked={checked}
+        onChange={onChange}
+      ></input>
+      <span
+        className={classNames(styles.icon, styles[size], styles[color])}
+      ></span>
+      <span className={styles.label}>{label}</span>
+    </div>
+  );
+};
+
+export default Checkbox;
